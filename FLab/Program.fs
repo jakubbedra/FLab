48e9539a45ad0ab1aa67e13ebf0a9f4e868dd41f
jakubbedra <jakubbedra@wp.pl>
2022-11-10 15:56:49 +0100

    Initial commit

---
+++ FLab/Program.fs
@@ -0,0 +1,44 @@
+﻿#light
+
+[<Measure>] type PLN
+[<Measure>] type EUR
+
+let convertEurToPln(x: double<EUR>) = x * 5.0
+
+
+
+// Define the Address type which is used in the Person record
+and Address =
+  { Line1: string
+    Line2: string
+    PostCode: string
+    Occupant: Person }
+  
+
+type Client = {
+    Id: int     
+}
+and Person = {
+    Name: string
+    Surname: string
+    PESEL: int option
+}
+
+  
+ let rec person =
+  {
+      Name = "Person name"
+      Age = 12
+      Address =
+          {
+              Line1 = "line 1"
+              Line2 = "line 2"
+              PostCode = "abc123"
+              Occupant = person
+          }
+  }
+
+
+printfn "Hello from F#"
+
+
