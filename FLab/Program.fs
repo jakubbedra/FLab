28ae9659334ca89e094930a7bbe0ac8289aa88df
jakubbedra <jakubbedra@wp.pl>
2022-11-10 16:02:42 +0100

    .

--- FLab/Program.fs
+++ FLab/Program.fs
@@ -1,22 +1,21 @@
 ﻿#light
 
+open System
+
 [<Measure>] type PLN
 [<Measure>] type EUR
 
-let convertEurToPln(x: double<EUR>) = x * 5.0
+let convertEurToPln(x: decimal<EUR>) = x * 5.0m
 
 
 
 // Define the Address type which is used in the Person record
-and Address =
-  { Line1: string
-    Line2: string
-    PostCode: string
-    Occupant: Person }
-  
 
 type Client = {
-    Id: int     
+    Id: int
+    MaxCredit: decimal // PLN
+    RegistrationDate: DateTime
+    Person: Person option
 }
 and Person = {
     Name: string
@@ -25,18 +24,18 @@ and Person = {
 }
 
   
- let rec person =
-  {
-      Name = "Person name"
-      Age = 12
-      Address =
-          {
-              Line1 = "line 1"
-              Line2 = "line 2"
-              PostCode = "abc123"
-              Occupant = person
-          }
-  }
+let rec person =
+{
+    Name = "Person name"
+    Age = 12
+    Address =
+    {
+        Line1 = "line 1"
+        Line2 = "line 2"
+        PostCode = "abc123"
+        Occupant = person
+    }
+}
 
 
 printfn "Hello from F#"
