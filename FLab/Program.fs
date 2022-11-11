c7fb2006a64daf75e17a9fb3eaeff4e000ec1e00
jakubbedra <jakubbedra@wp.pl>
2022-11-11 10:27:57 +0100

    pizda

--- FLab/Program.fs
+++ FLab/Program.fs
@@ -8,8 +8,11 @@ type PLN
 [<Measure>]
 type EUR
 
-let convertEurToPln = LanguagePrimitives.DecimalWithMeasure<PLN/EUR>(5.0m)
-let convertPlnToEur = LanguagePrimitives.DecimalWithMeasure<EUR/PLN>(0.2m)
+let convertEurToPln =
+    LanguagePrimitives.DecimalWithMeasure<PLN / EUR>(5.0m)
+
+let convertPlnToEur =
+    LanguagePrimitives.DecimalWithMeasure<EUR / PLN>(0.2m)
 
 let promotionThresholdPln = 2137m<PLN>
 let promotionThresholdDays: int = 20
@@ -34,9 +37,51 @@ and Person =
 
 type Transaction = { cost: decimal<EUR>; paid: bool }
 
+let trans0 =
+    { cost = 2169m<EUR>; paid = true }
+
+let trans1 =
+    { cost = 1234m<EUR>; paid = false }
+
+let trans2 =
+    { cost = 21.69m<EUR>; paid = true }
+
+let trans3 =
+    { cost = 12.69m<EUR>; paid = false }
+
+let trans4 =
+    { cost = 420.37m<EUR>; paid = true }
+
+let trans5 =
+    { cost = 269m<EUR>; paid = true }
+
+let trans6 =
+    { cost = 1.69m<EUR>; paid = false }
+
+let trans7 =
+    { cost = 69.0m<EUR>; paid = true }
+
 type ClientTransactions =
     { ClientId: int
       Transactions: list<Transaction> }
+
+let ct0 =
+    { ClientId = 0
+      Transactions = [ trans0; trans1; trans2 ] }
+
+let ct1 =
+    { ClientId = 1
+      Transactions = [ trans3; trans4 ] }
+
+let ct2 =
+    { ClientId = 0
+      Transactions = [ trans5; trans6 ] }
+
+let ct3 =
+    { ClientId = 0
+      Transactions = [ trans7 ] }
+
+let clientTransactions = [ ct0; ct1; ct2; ct3 ]
 
 let person0 =
     { Name = "Adam"
@@ -57,14 +102,14 @@ let client0 =
     { Id = 0
       Type = Basic
       MaxCredit = 10000m<PLN>
-      RegistrationDate = DateTime.Now
+      RegistrationDate = new DateTime(2020, 1, 1)
       Person = Some person0 }
 
 let client1 =
     { Id = 1
       Type = Basic
       MaxCredit = 10m<PLN>
-      RegistrationDate = new DateTime(2020, 1, 1)
+      RegistrationDate = DateTime.UtcNow
       Person = Some person1 }
 
 let client2 =
@@ -114,14 +159,12 @@ let rec updateClient (client: Client) =
 
 let isOurClientForLongerThan (clientId: int, days: int) =
     let client = findClient clientId
-
     match client with
     | Some cl -> (DateTime.UtcNow - cl.RegistrationDate).Days > days
     | None -> false
 
 let increaseCredit (clientId, credit) =
     let client = findClient clientId
-
     match client with
     | Some cl ->
         Some
@@ -133,27 +176,58 @@ let increaseCredit (clientId, credit) =
     | None -> None
 
 type public Shop() =
+    member public this.increaseClientCreditBy(clientId: int, additional: decimal<PLN>) =
+        match increaseCredit(clientId, additional) with
+            | Some cl ->
+                updateClient cl
+                true
+            | None -> false
+    
+    member public this.findClient(clientId: int) = 
+        findClient clientId
+    
+    member public this.creditThresholdExceeded(clientId: int) =
+        match findClient clientId with
+            | Some cl -> this.getTotalMoneyNotPaid clientId > cl.MaxCredit
+            | None -> false
+        
     member public this.promoteClient(clientId: int) =
-        if isOurClientForLongerThan (clientId, promotionThresholdDays)
-            && this.getTotalMoneySpend(clientId) > promotionThresholdPln
+        if
+            isOurClientForLongerThan (clientId, promotionThresholdDays)
+            && this.getTotalMoneySpend (clientId) > promotionThresholdPln
         then
-            this.mapToPromotedClient clientId
+            match this.mapToPromotedClient clientId with
+            | Some cl ->
+                updateClient cl
+                true
+            | None -> false
         else
-            None
+            false
 
     member private this.getClientTransactions(clientId: int) =
         readTransactions ()
         |> Seq.tryFind (fun c -> c.ClientId = clientId)
-        
+
     member private this.getTotalMoneySpend(clientId: int) =
         match this.getClientTransactions clientId with
-            | Some t ->
-                let total = t.Transactions
-                            |> Seq.filter (fun tr -> tr.paid)
-                            |> Seq.sumBy (fun tr -> tr.cost)
-                total * convertEurToPln     
-            | None -> 0m<PLN>
-        
+        | Some t ->
+            let total =
+                t.Transactions
+                |> Seq.filter (fun tr -> tr.paid)
+                |> Seq.sumBy (fun tr -> tr.cost)
+            total * convertEurToPln
+        | None -> 0m<PLN>
+
+    member private this.getTotalMoneyNotPaid(clientId: int) =
+        match this.getClientTransactions clientId with
+        | Some t ->
+            let total =
+                t.Transactions
+                |> Seq.filter (fun tr -> tr.paid = false)
+                |> Seq.sumBy (fun tr -> tr.cost)
+            total * convertEurToPln
+        | None -> 0m<PLN>
+    
     member private this.mapToPromotedClient(clientId: int) =
         match findClient clientId with
         | Some cl ->
@@ -166,13 +240,47 @@ type public Shop() =
         | None -> None
 
 // main
+updateTransactions clientTransactions
 updateClients clients
 
-//printfn $"{clients}"
 let shop = new Shop()
 
-//updateClient (shop.promoteClient(0))
-
-let clientsList = readClients ()
-
-printfn "%A" clientsList
+while true do
+    Console.Clear ()
+    printfn "1. Show client"
+    printfn "2. Increase max credit"
+    printfn "3. Check credit"
+    printfn "4. Promote client"
+    let number: string = Console.ReadLine ()
+    if System.String.Equals (number, "1") then
+        let num = Console.ReadLine ()
+        let ind = System.Int32.Parse num
+        match shop.findClient(ind) with
+        | Some cl -> printfn $"{cl}"
+        | None -> ()
+    elif System.String.Equals(number, "2") then
+        let num = Console.ReadLine ()
+        let ind = System.Int32.Parse num
+        let ac = Console.ReadLine ()
+        let additionalCredit: decimal = System.Decimal.Parse ac
+        if shop.increaseClientCreditBy (ind, additionalCredit * 1.0m<PLN>) then
+            printfn "Credit increased."
+        else
+            printfn "Could not find client with given id."
+    elif System.String.Equals(number, "3") then
+        let num = Console.ReadLine ()
+        let ind = System.Int32.Parse num
+        if shop.creditThresholdExceeded ind then
+            printfn "Client with the given id has exceeded the credit threshold."
+        else
+            printfn "Client with the given id has not exceeded the credit threshold."
+    elif System.String.Equals(number, "4") then
+        let num = Console.ReadLine ()
+        let ind = System.Int32.Parse num
+        if shop.promoteClient(ind) then
+            printfn "Client promoted!"
+        else
+            printfn "Client could not be promoted."
+    Console.ReadLine ()
+    
+    
